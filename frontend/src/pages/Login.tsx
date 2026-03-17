9504407e2efde8933cefc9ff229b42ffdcef407d
Cortega2-1 <cortega2@mail.umw.edu>
2026-03-17 08:44:07 -0400

    clean code

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
 import { Shield, Lock, AlertCircle, Mail } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
-import { supabase } from '../lib/supabaseClient'; // adjust path if needed
+import { supabase } from '../lib/supabaseClient';
 
 interface FormData {
     email: string;
